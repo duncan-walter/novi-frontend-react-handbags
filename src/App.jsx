094e3e2199a2ee48d6871825161b3686fc59db47
Duncan Walter <dwalter@snelstart.nl>
2025-05-20 08:55:56 +0200

    added nav with button without component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
 
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  )
+  function logButtonText(e) {
+    console.log(e.target.textContent);
+  }
+
+  return (<>
+    <h1>Handbags & Purses</h1>
+    <nav>
+      <button onClick={logButtonText}>to the collection</button>
+      <button onClick={logButtonText}>shop all bags</button>
+      <button onClick={logButtonText} disabled={true}>pre-order</button>
+    </nav>
+  </>)
 }
 
 export default App
