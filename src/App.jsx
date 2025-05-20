2ce1e474fab15e9cc5508cbd9df89a13e93cd7bd
Duncan Walter <duncan1997@hotmail.nl>
2025-05-20 09:38:02 +0200

    added footer with tiles without component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Bag2Image from './assets/bag_2.png';
 import Bag3Image from './assets/bag_3.png';
 import Bag4Image from './assets/bag_4.png';
 
+import Tile1Image from './assets/brand.png';
+import Tile2Image from './assets/our_story.png';
+
 function App() {
   function logButtonText(e) {
     console.log(e.target.textContent);
@@ -49,6 +52,33 @@ function App() {
         productPrice="150"
       />
     </main>
+    <footer>
+      <section>
+        <h2>THE BRAND</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid culpa dolorum enim excepturi, fuga pariatur
+          praesentium quia sequi similique sunt.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid culpa dolorum enim excepturi, fuga pariatur
+          praesentium quia sequi similique sunt.
+        </p>
+      </section>
+      <section>
+        <img src={Tile1Image} alt="Brand"/>
+      </section>
+      <section>
+        <img src={Tile2Image} alt="Our story"/>
+      </section>
+      <section>
+        <h2>OUR STORY</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis eligendi exercitationem illo, labore
+          laboriosam nihil omnis praesentium. Aspernatur cum deleniti excepturi itaque, laboriosam nisi rerum sunt. At
+          repellendus tenetur veniam!
+        </p>
+      </section>
+    </footer>
   </>)
 }
 
