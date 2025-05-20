68ea34ad4bd4e3760d1b611619ac3d539c566a94
Duncan Walter <duncan1997@hotmail.nl>
2025-05-20 09:20:07 +0200

    added main with products without component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import './App.css'
-import Button from "./components/Button.jsx";
+import Button from './components/Button.jsx';
+
+import Bag1Image from './assets/bag_1.png';
+import Bag2Image from './assets/bag_2.png';
+import Bag3Image from './assets/bag_3.png';
+import Bag4Image from './assets/bag_4.png';
 
 function App() {
   function logButtonText(e) {
@@ -13,6 +18,32 @@ function App() {
       <Button buttonText="shop all bags" action={logButtonText} />
       <Button buttonText="pre-order" action={logButtonText} isDisabled={true} />
     </nav>
+    <main>
+      <article>
+        <span>Best seller</span>
+        <img src={Bag1Image} alt="Handy bag"/>
+        <p>The handy bag</p>
+        <h4>€400,-</h4>
+      </article>
+      <article>
+        <span>Best seller</span>
+        <img src={Bag2Image} alt="Stylish bag"/>
+        <p>The stylish bag</p>
+        <h4>€250,-</h4>
+      </article>
+      <article>
+        <span>New collection</span>
+        <img src={Bag3Image} alt="Simple bag"/>
+        <p>The simple bag</p>
+        <h4>€300,-</h4>
+      </article>
+      <article>
+        <span>New collection</span>
+        <img src={Bag4Image} alt="Trendy bag"/>
+        <p>The trendy bag</p>
+        <h4>€150,-</h4>
+      </article>
+    </main>
   </>)
 }
 
